17b1d03cbcca3479ec349f94dea32a0b3beb9190
Alperen Yilmaz <alp-eren.42@outlook.com>
2023-05-14 12:58:10 +0200

    Save passwords inside .json file

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-# high security password generator in python, written by saint
-# Copyright © Alperen Yilmaz aka. vali-github 2022 - All Rights Reserved
-
-import random
+import json
+import os
 import string
+import random
 
-def generate_password(length):
-    # Generate a random password with multiple strings with 16 chars (ascii, digits, punctuation)
-    password = string.ascii_letters + string.digits + string.punctuation
-    return ''.join(random.choice(password) for i in range(length))
+def generate_password(length=16):
+    chars = string.ascii_letters + string.digits + string.punctuation
+    chars = chars.replace('\\', '').replace('"', '').replace("'", "")
+    password = ''.join(random.choice(chars) for i in range(length))
+    return password
 
-print(generate_password(16))
+def save_password(website, username, password):
+    data = {}
+    if os.path.exists("passwords.json"):
+        with open("passwords.json", "r") as f:
+            data = json.load(f)
+    data[website] = {"username/email": username, "password": password}
+    with open("passwords.json", "w") as f:
+        json.dump(data, f, indent=4, default=str)
+
+def main():
+    website = input("Enter the website for which you want to generate a password: ")
+    username = input("Enter the username/email for which you want to generate a password: ")
+    
+    password = generate_password()
+    save_password(website, username, password)
+
+    print(f"Generated password: {password}\n")
+    print("Your details have been saved inside the passwords.json file!")
+
+if __name__ == "__main__":
+    main()
